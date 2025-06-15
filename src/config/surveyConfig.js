423b1e000c6e17bca2fffdbefd5127e85a50aecd
Lukas Cocinero <koch@dksr.city>
2025-06-15 20:57:52 +0200

    dksr image

--- src/config/surveyConfig.js
+++ src/config/surveyConfig.js
@@ -1,10 +1,10 @@
 export const surveyConfig = {
   // Basic Information
-  title: "Umfrage - Maschinelles Lernen menschlicher Sichtweisen",
+  title: "Maschinelles Lernen menschlicher Sichtweisen",
   description: "Hallo! Wir vom City Intelligence Team bei DKSR untersuchen derzeit, wie Menschen gebaute Straßenräume wahrnehmen. Diese Daten helfen uns, Orte zu identifizieren, die als angenehm oder unangenehm, sicher oder unsicher empfunden werden – zum Beispiel sogenannte Angsträume oder Orte mit wenig Aufenthaltsqualität. Ziel ist es, urbane Räume besser zu gestalten, sodass sie sich für alle Menschen gut anfühlen. Die Umfrage dauert etwa 15 Minuten und basiert auf Straßenbildern, die Sie visuell beurteilen. Ihre Teilnahme unterstützt eine menschzentrierte Stadtplanung.",
   
   // Lab Information - Replace with your own
-  logo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dksr.city%2F&psig=AOvVaw33CZhlZizKDDLvmGiJQmml&ust=1750096809589000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNjn4eaA9I0DFQAAAAAdAAAAABAE",
+  logo: "https://www.dksr.city/wp-content/uploads/2024/09/DKSR-Logo-2024-512px.png",
   logoPosition: "right",
   labDescription: "Diese Forschung wird vom City Intelligence Team der Data Competence Center for Cities and Regions (DKSR) durchgeführt.",
   
